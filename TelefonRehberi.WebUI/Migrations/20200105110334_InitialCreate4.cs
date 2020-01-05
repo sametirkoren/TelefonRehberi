4949c78efe3212d5f93d3f961a59a03e24b0bcc3
Samet İrkören <sametirkoren@gmail.com>
2020-01-05 14:27:15 +0300

    cdn bağımlı hale getirildi.

---
+++ TelefonRehberi.WebUI/Migrations/20200105110334_InitialCreate4.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TelefonRehberi.WebUI.Migrations
+{
+    public partial class InitialCreate4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
